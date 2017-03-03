6dd2c6087fbc876f3c2a3d8e98fe987b0e1a91ac
Lic. José Ángel Peña Godínez <licangelgodinez@hotmail.com>
2017-03-03 00:24:09 -0600

    Más drawables y primera prueba

--- app/src/main/java/com/gigasoft/csalpha_v1_0_0/MainActivity.java
+++ app/src/main/java/com/gigasoft/csalpha_v1_0_0/MainActivity.java
@@ -2,12 +2,35 @@ package com.gigasoft.csalpha_v1_0_0;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView shopng, histo, frnds, quick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        shopng = (TextView) findViewById(R.id.tvDescripcionTarjeta);
+        histo = (TextView) findViewById(R.id.tvDescripcionTarjeta1);
+        frnds = (TextView) findViewById(R.id.tvDescripcionTarjeta2);
+        quick = (TextView) findViewById(R.id.tvDescripcionTarjeta3);
+
+
+    }
+
+    private void testingAction() {
+        final TextView chose = new TextView(this);
+        chose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),chose.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
